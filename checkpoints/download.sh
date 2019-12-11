3c51f38a9f0ec619390b916e287edfd12ce6a4b7
shuaix <shuaixie@zju.edu.cn>
2019-12-11 18:08:42 +0800

    update

--- checkpoints/download.sh
+++ checkpoints/download.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 
 cd checkpoints
 
 # Download Trancos checkpoint
-curl -L https://www.dropbox.com/sh/rms4dg5autwtpnf/AADQBOr1ruFsptbqG_uPt_zCa?dl=0 > trancos_ResFCN.zip
-mkdir trancos_ResFCN
-unzip trancos_ResFCN.zip -d trancos_ResFCN
-rm trancos_ResFCN.zip
-mv trancos_ResFCN/State_Dicts/best_model.pth best_model_trancos_ResFCN.pth
-mv trancos_ResFCN/history.json history_trancos_ResFCN.json
-rm -r trancos_ResFCN
+#curl -L https://www.dropbox.com/sh/rms4dg5autwtpnf/AADQBOr1ruFsptbqG_uPt_zCa?dl=0 > trancos_ResFCN.zip
+#mkdir trancos_ResFCN
+#unzip trancos_ResFCN.zip -d trancos_ResFCN
+#rm trancos_ResFCN.zip
+#mv trancos_ResFCN/State_Dicts/best_model.pth best_model_trancos_ResFCN.pth
+#mv trancos_ResFCN/history.json history_trancos_ResFCN.json
+#rm -r trancos_ResFCN
 
 # Download Pascal checkpoint
 curl -L https://www.dropbox.com/sh/pwmoej499sfqb08/AABY13YraHYF51yw62Zc1w0-a?dl=0 > pascal_ResFCN.zip
